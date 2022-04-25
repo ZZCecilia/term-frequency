8f58a94945cc7ac771b59e91c83a22fe77e253ab
ZZCecilia <87951499+ZZCecilia@users.noreply.github.com>
2022-04-24 23:18:32 -0400

    Update termfrequency.b.R

--- termfrequency/R/termfrequency.b.R
+++ termfrequency/R/termfrequency.b.R
@@ -5,38 +5,64 @@ termfrequencyClass <- if (requireNamespace('jmvcore', quietly=TRUE)) R6::R6Class
   inherit = termfrequencyBase,
   private = list(
     .run = function() {
-     
+      
       #Reading the text file and defining the data object
       text1 <- self$data
       
       #Connecting R and Jamovi(options)
+      fretext <- self$option$fretext
       dep <- self$option$dep
       wcpa <- self$option$wcpa
-      hmwtd <- self$option$hmtd
+      hmwtd <- self$option$hmwtd
+
+      ### Defined Functions ###
+      # Pre-processing data with defined function
+      corpus_preprocessing = function(corpus){
+        # Replace special symbols with space 
+        toSpace <- content_transformer(function (x , pattern) gsub(pattern, " ", x))
+        # Normalization
+        corpus <- tm_map(corpus, toSpace, "/")
+        corpus <- tm_map(corpus,toSpace,"@")
+        corpus <- tm_map(corpus,toSpace,"\\ï½")
+        corpus <- tm_map(corpus,toSpace,"#")
+        corpus <- tm_map(corpus, toSpace, "Â®")
+        # Casing (upper case & lower case), convert the text to lower case
+        corpus <- tm_map(corpus, content_transformer(tolower))
+        # Remove punctuation 
+        corpus <- tm_map(corpus, removePunctuation)
+        # Remove extra white space 
+        corpus <- tm_map(corpus, stripWhitespace)
+        # Remove Stop words
+        corpus <- tm_map(corpus,removeWords,stopwords("english"))
+        corpus <- tm_map(corpus,removeWords,c("the","and","The","And","A","An","a","an","e","d"))
+        # Stemming (e.g. -ing vs original)
+        corpus <- tm_map(corpus,stemDocument, language ="english")
+        return(corpus)
+      }
       
-      # review_text <- paste(reviews$Reviews, collapse=" ")
+      #Initialize Data variables
+      #Creating Data Object
+      if (is.null(self$options$fretext)) 
+        return()
       
-      #Setting up and loading the data as a corpus
-      text_corpus <- Corpus(VectorSource(text1))
-      # review_source <- VectorSource(review_text) # need source for the text
-      # corpus <- Corpus(review_source) # corpus is the name for a collection of documents that want to compare
-      # 
+      fre_df<- data.frame(
+        d_fretext<- self$data[[self$options$fretext]])
       
-      #Cleaning up the text data
-      text_corpus <- tm_map(text_corpus, stripWhitespace)
-      text_corpus <- tm_map(text_corpus, content_transformer(tolower))
-      text_corpus <- tm_map(text_corpus, removePunctuation)
-      text_corpus <- tm_map(text_corpus, removeNumbers)
-      text_corpus <- tm_map(text_corpus, removeWords, stopwords("english"))
+      # Transfer the data into corpus 
+      text_corpus = VCorpus(DataframeSource(fre_df))
       
+      # Apply data pre-processing to corpus
+      corpus_cleaned <- corpus_preprocessing(text_corpus)
       
       #Building a document-term matrix (DTM)
-      text_corpus_dtm <- DocumentTermMatrix(text_corpus)
-      text_corpus_dtm2 <- as.matrix(text_corpus_dtm)
+      text_dtm <- DocumentTermMatrix(corpus_cleaned)
+      dtm_fre <- as.matrix(text_dtm)
+      
       
       #Word Frequency
-      words_frequency <- colSums(as.matrix(text_corpus_dtm2))
+      words_frequency <- colSums(dtm_fre)
       words_frequency <- sort(words_frequency, decreasing = TRUE)
+      
       
       #Display the result
       result <- print(words_frequency)
@@ -50,3 +76,6 @@ termfrequencyClass <- if (requireNamespace('jmvcore', quietly=TRUE)) R6::R6Class
       
     })
 )
+
+
+
